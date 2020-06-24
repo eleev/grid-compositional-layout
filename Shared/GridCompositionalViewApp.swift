6ecc1abdb49fc2d32454eae26d19e19dd80ccf25
Astemir Eleev <astemireleev@gmail.com>
2020-06-24 14:23:57 +0300

    Initial Commit

---
+++ Shared/GridCompositionalViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCompositionalViewApp.swift
+//  Shared
+//
+//  Created by Astemir Eleev on 24.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct GridCompositionalViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
