0eebe00dbde4d386d455f2f300c3eade2e1be215
Astemir Eleev <astemireleev@gmail.com>
2020-06-29 18:00:40 +0300

    Excluded non-framework directories and assets

--- Package.swift
+++ Package.swift
@@ -21,10 +21,14 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GridCompositionalLayout",
-            dependencies: []),
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.GridCompositionalLayout
+        .target(name: "GridCompositionalLayout",
+                dependencies: [],
+                exclude: [
+                    "Assets",
+                    "Demo",
+                    "logo.jpg"
+                ]),
         .testTarget(
             name: "GridCompositionalLayoutTests",
             dependencies: ["GridCompositionalLayout"]),
